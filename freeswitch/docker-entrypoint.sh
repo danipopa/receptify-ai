b903d148f4b2b45595561596ac99260bd1a6ee4b
Dani Popa <dani.popa@gmail.com>
2026-05-04 14:14:33 -0700

    feat(freeswitch): build from source with mod_audio_stream; fix config paths

--- freeswitch/docker-entrypoint.sh
+++ freeswitch/docker-entrypoint.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
 set -e
 
-VARS_XML=/etc/freeswitch/vars.xml
+VARS_XML=/usr/local/freeswitch/conf/vars.xml
+ESL_CONF=/usr/local/freeswitch/conf/autoload_configs/event_socket.conf.xml
 
-# Inject external SIP/RTP IP from env vars into vars.xml
+# Inject external SIP/RTP IP
 if [ -n "$FS_EXT_SIP_IP" ] && [ -f "$VARS_XML" ]; then
   sed -i 's|data="ext-sip-ip=[^"]*"|data="ext-sip-ip='"${FS_EXT_SIP_IP}"'"|' "$VARS_XML"
+  sed -i 's|data="ext-rtp-ip=[^"]*"|data="ext-rtp-ip='"${FS_EXT_SIP_IP}"'"|' "$VARS_XML"
 fi
 
 if [ -n "$FS_EXT_RTP_IP" ] && [ -f "$VARS_XML" ]; then
   sed -i 's|data="ext-rtp-ip=[^"]*"|data="ext-rtp-ip='"${FS_EXT_RTP_IP}"'"|' "$VARS_XML"
-else
-  # default ext-rtp-ip to ext-sip-ip if not set separately
-  if [ -n "$FS_EXT_SIP_IP" ] && [ -f "$VARS_XML" ]; then
-    sed -i 's|data="ext-rtp-ip=[^"]*"|data="ext-rtp-ip='"${FS_EXT_SIP_IP}"'"|' "$VARS_XML"
-  fi
 fi
 
 # Inject ESL password
-if [ -n "$FS_ESL_PASSWORD" ]; then
-  sed -i 's|<param name="password" value="[^"]*"/>|<param name="password" value="'"${FS_ESL_PASSWORD}"'"/>|' \
-    /etc/freeswitch/autoload_configs/event_socket.conf.xml
+if [ -n "$FS_ESL_PASSWORD" ] && [ -f "$ESL_CONF" ]; then
+  sed -i 's|<param name="password" value="[^"]*"/>|<param name="password" value="'"${FS_ESL_PASSWORD}"'"/>|' "$ESL_CONF"
 fi
 
-exec /usr/bin/freeswitch "$@"
+exec /usr/local/freeswitch/bin/freeswitch "$@"
